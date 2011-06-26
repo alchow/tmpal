2e302f04ff2e5866857357cc990919ee6c056908
Albert Chow <alcchow@gmail.com>
2011-06-26 00:34:40 -0400

    test

--- index.php
+++ index.php
@@ -1,1 +1,47 @@
-hey there
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8"> 
+	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+	<title>jQuery Mobile: Demos and Documentation</title> 
+	<link rel="stylesheet"  href="http://code.jquery.com/mobile/1.0b1/jquery.mobile-1.0b1.min.css" />
+	<link rel="stylesheet"  href="style/gallery.css" /> 
+	<script src="http://code.jquery.com/jquery-1.6.1.min.js"></script> 
+	<script src="http://code.jquery.com/mobile/1.0b1/jquery.mobile-1.0b1.min.js"></script> 
+</head>
+<body>
+	<div data-role="page" id="jqm-home" class="type-home"> 
+		<div data-role="content">
+			<div class="content-primary"> 
+				<nav> 
+
+
+							<ul data-role="listview" data-theme="c" data-dividertheme="b"> 
+								<li>
+									<img src="img/pic1.png" alt="pic1"/>
+									 <a href="site.php?siteName=nettuts">pic1</a> 
+								</li> 
+								<li>
+									<img src="img/pic2.png" alt="pic2"/>
+									 <a href="site.php?siteName=nettuts">pic2</a> 
+								</li>
+								<li>
+									<img src="img/pic3.png" alt="pic3"/>
+									 <a href="site.php?siteName=nettuts">pic3</a> 
+								</li>
+								<li>
+									<img src="img/pic4.png" alt="pic4"/>
+									 <a href="site.php?siteName=nettuts">pic4</a> 
+								</li>
+								<li>
+									<img src="img/pic5.png" alt="pic5"/>
+									 <a href="site.php?siteName=nettuts">pic5</a> 
+								</li>
+
+							</ul> 
+				</nav> 
+			</div>
+		</div>
+	</div>
+</body>
+</html
